02ff05fd715fff05702b9b1feed2e17f546367af
Lars Haugeli <lhhaugel@hiof.no>
2019-04-30 21:09:01 +0200

    Popular podcasts

--- Podbase.APP/ViewModels/MainViewModel.cs
+++ Podbase.APP/ViewModels/MainViewModel.cs
@@ -1,13 +1,34 @@
 ﻿using System;
-
+using System.Collections.Generic;
 using Podbase.APP.Helpers;
+using Podbase.Model;
 
 namespace Podbase.APP.ViewModels
 {
     public class MainViewModel : Observable
     {
+        public List<Podcast> PopularPodcasts { get; set; }
+
         public MainViewModel()
         {
+            PopularPodcasts = new List<Podcast>
+            {
+                new Podcast()
+                {
+                    Creator = "This American Life",
+                    Name = "Serial",
+                    Genre = "True crime",
+                    Description = "True crime about a murder."
+                },
+
+                new Podcast()
+                {
+                    Creator = "Reply All",
+                    Name = "ABC",
+                    Genre = "Entertainment",
+                    Description = "Podcast about the internet."
+                }
+            };
         }
 
         private string _username = LoginViewModel.loggedInUsername;
