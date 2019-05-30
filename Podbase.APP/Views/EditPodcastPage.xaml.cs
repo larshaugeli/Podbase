3eae0b400e46ca829c8936e57da1637d5a2710ba
Lars Haugeli <lhhaugel@hiof.no>
2019-05-30 19:50:58 +0200

    binding to x:bind

--- Podbase.APP/Views/EditPodcastPage.xaml.cs
+++ Podbase.APP/Views/EditPodcastPage.xaml.cs
@@ -1,15 +1,12 @@
 ﻿using Windows.UI.Xaml.Controls;
 using Podbase.APP.ViewModels;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace Podbase.APP.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class EditPodcastPage : Page
     {
+        public EditPodcastViewModel ViewModel { get; } = new EditPodcastViewModel();
+
         public EditPodcastPage()
         {
             InitializeComponent();
